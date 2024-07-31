d9e1c9bbf8a4ab069287eb897932fcfdcae16df1
Gokul Anand <mgokulanand007@gmail.com>
2024-07-31 13:04:38 +0530

    File Uploaded

---
+++ src/test/java/com/springboot/assessment/CodeQualityAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeQualityAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
